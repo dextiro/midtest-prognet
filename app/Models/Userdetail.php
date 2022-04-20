c343398611a28ac64abe65a2743b12c5c722ec9f
Kadek Deksy Dextiro <75655008+dextiro@users.noreply.github.com>
2022-04-20 17:24:16 +0800

    Update Userdetail.php

--- app/Models/Userdetail.php
+++ app/Models/Userdetail.php
@@ -9,4 +9,11 @@ class Userdetail extends Model
 {
     protected $fillable = [ 'id' ];
     use HasFactory;
+    
+    public function mahasiswa(){
+        return $this->belongsTo(Mahasiswa::class, 'id_mahasiswa');
+    }
+    public function kelompok_studi(){
+        return $this->belongsTo(KelompokStudi::class, 'id_kelompokstudi');
+    }
 }
